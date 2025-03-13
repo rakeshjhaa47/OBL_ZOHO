cb6e7b951040aff30e172c0b9cd3b4e3d4b8aa8d
rakeshjhaa47 <rakeshjhaa47@gmail.com>
2025-03-13 15:49:38 +0530

    changes

--- Models/Response/CpConfirmResponse.cs
+++ Models/Response/CpConfirmResponse.cs
@@ -14,10 +14,13 @@
 
     public class CpData
     {
+        public string CP_Confirmed_the_Sale { get; set; }
+        public string Deal_Name { get; set; }
         public int Amount { get; set; }
         public string id { get; set; }
         public string Assigned_CP_Name { get; set; }
         public string Closing_Date { get; set; }
+
     }
 
     public class CpInfo
